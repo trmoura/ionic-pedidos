88c90e1b7e7a5698d34c9d521614b73a10cfbab8
Tiago Moura <trmoura@yahoo.com.br>
2018-03-11 11:00:58 -0300

    desabilitando menu na home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController , IonicPage } from 'ionic-angular';
+import { NavController, IonicPage } from 'ionic-angular';
+import { MenuController } from 'ionic-angular/components/app/menu-controller';
 
 @IonicPage()
 @Component({
@@ -8,11 +9,18 @@ import { NavController , IonicPage } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public menu: MenuController) {
 
   }
 
-  login(){
+  ionViewWillEnter() {
+    this.menu.swipeEnable(false);
+  }
+  ionViewDidLeave() {
+    this.menu.swipeEnable(true);
+  }
+
+  login() {
     this.navCtrl.setRoot('CategoriasPage');
   }
 
