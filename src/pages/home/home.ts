8f9975b83b75d54de528c28ca9c832f62320a0b5
Tiago Moura <trmoura@yahoo.com.br>
2018-03-12 19:41:32 -0300

    pegando dados do formulario de login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
 import { MenuController } from 'ionic-angular/components/app/menu-controller';
+import { CredenciaisDTO } from '../../models/credenciais.dto';
 
 @IonicPage()
 @Component({
@@ -9,6 +10,11 @@ import { MenuController } from 'ionic-angular/components/app/menu-controller';
 })
 export class HomePage {
 
+
+  creds: CredenciaisDTO = {
+    email: "",
+    senha: ""
+  };
   constructor(public navCtrl: NavController, public menu: MenuController) {
 
   }
@@ -21,6 +27,7 @@ export class HomePage {
   }
 
   login() {
+    console.log(this.creds);
     this.navCtrl.setRoot('CategoriasPage');
   }
 
